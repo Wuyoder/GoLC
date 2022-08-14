8e0dccca859dd130075bca15eb436f60f4dd3e22
Wuyoder <namnus1993@gmail.com>
2022-08-14 17:02:52 +0800

    001

---
+++ 001twoSum.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+var nums = []int{2, 7, 11, 15}
+var target = 9
+
+func twoSum(nums []int, target int) []int {
+	x := []int{}
+	var remain int
+	for i := 0; i < len(nums); i++ {
+		remain = target - nums[i]
+		for j := i + 1; j < len(nums); j++ {
+			if nums[j] == remain {
+				x = append(x, i, j)
+			}
+		}
+	}
+	return x
+}
+
+func main() {
+	fmt.Println("TwoSum")
+	fmt.Println(twoSum(nums, target))
+
+}
